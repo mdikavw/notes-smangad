8a8aed5c8bcd154ba1f3277950eca4d3de7b6d4b
elangrama22 <elangrama22@gmail.com>
2026-03-07 09:12:55 +0700

    register ricing

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -27,8 +27,21 @@ export default function RegisterPage() {
     };
 
     return (
-        <div className="flex h-[80vh] items-center justify-center">
-            <form onSubmit={handleSubmit} className="bg-white p-8 rounded-2xl shadow-xl w-96 flex flex-col gap-4 border border-slate-200">
+        <div className="absolute inset-0 flex items-center justify-center bg-[#262e6d] overflow-hidden">
+            
+            <div 
+                className="absolute inset-0 z-0 opacity-10"
+                style={{
+                    backgroundImage: `
+                        linear-gradient(#ffc65c 1px, transparent 2px),
+                        linear-gradient(90deg, #ffc65c 1px, transparent 2px)
+                    `,
+                    backgroundSize: '40px 40px',
+                    backgroundPosition: 'center center'
+                }}
+            ></div>
+
+            <form onSubmit={handleSubmit} className="relative z-10 bg-white p-8 rounded-2xl shadow-xl w-96 flex flex-col gap-4 border border-slate-200">
                 <h1 className="text-2xl font-black text-[#262e6d] text-center">Buat Akun Baru</h1>
                 <p className="text-sm text-center text-slate-500">Daftarkan akun untuk masuk ke Dashboard</p>
                 
