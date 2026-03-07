dc38dfa6a0cdb7e447f17eed4eb3ae0d7c34c083
elangrama22 <elangrama22@gmail.com>
2026-03-07 07:55:50 +0700

    Initial setup

---
+++ src/app/actions/register.ts
@@ -0,0 +1,24 @@
+"use server";
+import { prisma } from "@/lib/prisma"; 
+import bcrypt from "bcrypt";
+
+export async function registerUser(formData: FormData) {
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  try {
+    // Hash password sebelum simpan ke MySQL
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    await prisma.user.create({
+      data: { 
+        email, 
+        password: hashedPassword 
+      },
+    });
+    return { success: true };
+  } catch (error) {
+    console.error(error);
+    return { success: false, error: "Email sudah terdaftar!" };
+  }
+}
